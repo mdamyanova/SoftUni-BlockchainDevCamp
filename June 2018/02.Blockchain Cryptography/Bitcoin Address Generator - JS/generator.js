548c35c368f5acfce89a72db3f8f90cabfbf5805
mdamyanova <mdamyanova181@gmail.com>
2018-06-20 02:59:26 +0300

    edit

--- June 2018/02.Blockchain Cryptography/Bitcoin Address Generator - JS/generator.js
+++
@@ -1,9 +0,0 @@
-var bitcoin = require('bitcoinjs-lib');
-
-var keyPair = bitcoin.ECPair.makeRandom();
-
-var privateKey = keyPair.toWIF();
-console.log(privateKey);
-
-var publicKey = keyPair.getAddress();
-console.log(publicKey);
